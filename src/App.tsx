36946c7fe726767cde89b382767c041eb7c1f557
jpbhatt21 <jatan.229301400@muj.manipal.edu>
2025-02-03 18:59:53 +0530

    add piece movements

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { moveManager } from "./PieceMoves";
 let md = false;
 let pc = " -1";
 function boardPositionToGlobalPosition(k: any) {
-  let x = (8 - (k % 8) - 4.5) * 9.375 + 4;
-  let y = (Math.floor(k / 8) - 3.5) * 9.375 + 4;
+	let x = (8 - (k % 8) - 4.5) * 9.375 + 4;
+	let y = (Math.floor(k / 8) - 3.5) * 9.375 + 4;
 
-  return {
-    top: "calc(50vh - " + y + "vmin)",
-    left: "calc(50vw - " + x + "vmin)",
-  };
+	return {
+		top: "calc(50vh - " + y + "vmin)",
+		left: "calc(50vw - " + x + "vmin)",
+    opacity:k==-1?0:1,
+    pointerEvents:turn&&board[k]<"a"||!turn&&board[k]>="a"?"auto":"none",
+    transitionDuration:k==-1?"0s":"0.2s"
+
+	};
 }
-let board =
-  "RNBQKBNRPPPPPPPP--------------------------------pppppppprnbqkbnr";
+let board = "RNBQKBNRPPPPPPPP--------------------------------pppppppprnbqkbnr";
 let pieceKeys = [
 	[8, 9, 10, 11, 12, 13, 14, 15],
 	[0, 7],
@@ -27,173 +30,196 @@ let pieceKeys = [
 	[58, 61],
 	[59],
 	[60],
-  ];
-let sel=-1
+];
+let sel = -1;
 let turn = true;
+let mvSq = new Array(64).fill(0);
+function moveTo(i: any) {
+	if (i < 0 || i > 63) return false;
+	if (mvSq[i] == 0) return false;
+	pieceKeys = pieceKeys.map((x: any) => {
+		return x.map((y: any) => (y == i ? -1 : y == sel ? i : y));
+	});
+	mvSq = new Array(64).fill(0);
+	turn = !turn;
+	let temp = board.split("");
+	temp[i] = temp[sel];
+	temp[sel] = "-";
+	board = temp.join("");
+	return true;
+}
 function App() {
-  const [clickCount, setClickCount] = useState(0);
-  const [downPiece, setDownPiece] = useState(-1);
-  const [moveableSquares, setMoveableSquares] = useState(new Array(64).fill(0));
-  const [pieceID, setPieceID] = useState("null");
-  useEffect(() => {
-    document.addEventListener("mousemove", (e) => {
-      let piece = document.getElementById(pc);
-      if (!piece) {
-        return;
-      }
-      let boardDim = document
-        .getElementById("mainboard")
-        ?.getBoundingClientRect() || { x: 0, y: 0 };
+	const [clickCount, setClickCount] = useState(0);
+	const [moveableSquares, setMoveableSquares] = useState(
+		new Array(64).fill(0)
+	);
+	useEffect(() => {
+		document.addEventListener("mousemove", (e) => {
+			let piece = document.getElementById(pc);
+			if (!piece) {
+				return;
+			}
 
-      if (md) {
-        piece.style.top = "calc( " + e.clientY + "px - 4vmin)";
-        piece.style.left = "calc( " + e.clientX + "px - 4vmin)";
-        piece.style.transitionDuration = "0s";
-      }
-    });
+			if (md) {
+				piece.style.top = "calc( " + e.clientY + "px - 4vmin)";
+				piece.style.left = "calc( " + e.clientX + "px - 4vmin)";
+				piece.style.transitionDuration = "0s";
+			}
+		});
 
-    document.addEventListener("mouseup", () => {
-      let i = downPiece - 1;
-      setDownPiece(-1);
-      md = false;
-      let piece = document.getElementById(pc);
-      if (!piece) {
-        return;
-      }
-	  console.log(pc)
-      pc = "-1" 
-	  setPieceID("null");
-      piece.style.transitionDuration = "0.2s";
-      let val = boardPositionToGlobalPosition(i + 1);
-      piece.style.top = val.top;
-      piece.style.left = val.left;
-    });
-  });
-  console.log(pieceKeys);
-  return (
-    <>
-      <div className="fixed flex w-full h-full items-center justify-center flex-col">
-        <svg
-          id="mainboard"
-          xmlns="http://www.w3.org/2000/svg"
-          className=" absolute h-[75vmin] aspect-square tms duration-200  mb-0 "
-          viewBox="0 0 640 640"
-          fill="none"
-        >
-          {moveableSquares.map((x: any, i) => {
-            return (
-              <>
-                <rect
-                  key={i}
-                  x={(i % 8) * 80}
-                  y={560 - Math.floor(i / 8) * 80}
-                  width="80"
-                  height="80"
-                  fill={
-                    (i + Math.floor(i / 8)) % 2 == 1
-                      ? theme.whiteBoard
-                      : theme.blackBoard
-                  }
-                  stroke="#000"
-                  strokeWidth="1"
-                  onClick={() => {
-                   
-                  }}
-                />
+		document.addEventListener("mouseup", (e) => {
+			let i = sel;
+			md = false;
+			let piece = document.getElementById(pc);
+			let to: any = (e.target as HTMLElement) || { id: "-1" };
+			if (to.id.includes("boardsq")) {
+				to = parseInt(to.id.replace("boardsq", ""));
+			} else {
+				to = -1;
+			}
+
+			let val1 = boardPositionToGlobalPosition(to);
+			let val2 = boardPositionToGlobalPosition(i);
+			let boox = moveTo(to);
+
+			setMoveableSquares(mvSq);
+			if (!piece) {
+				return;
+			}
+			pc = "-1";
+			piece.style.pointerEvents = "auto";
+			piece.style.transitionDuration = "0.2s";
+			let val = boox ? val1 : val2;
+			piece.style.top = val.top;
+			piece.style.left = val.left;
+		});
+	}, []);
+	return (
+		<>
+			<div className="fixed flex w-full h-full items-center justify-center flex-col">
+				<svg
+					id="mainboard"
+					xmlns="http://www.w3.org/2000/svg"
+					className=" absolute h-[75vmin] aspect-square tms duration-200  mb-0 "
+					viewBox="0 0 640 640"
+					fill="none">
+					{moveableSquares.map((_, i) => {
+						return (
+							<>
+								<rect
+									id={"boardsq" + i}
+									key={i}
+									x={(i % 8) * 80}
+									y={560 - Math.floor(i / 8) * 80}
+									width="80"
+									height="80"
+									fill={
+										(i + Math.floor(i / 8)) % 2 == 1
+											? theme.whiteBoard
+											: theme.blackBoard
+									}
+									stroke="#000"
+									strokeWidth="1"
+								/>
                 {
-                  <rect
-                    x={(i % 8) * 80 + 32}
-                    y={592 - Math.floor(i / 8) * 80}
-                    width="16"
-                    height="16"
-                    rx="999"
-                    fill={theme.move}
-                    className="duration-200"
-                    opacity={moveableSquares[i]}
-                    onClick={() => {
-						if(moveableSquares[i] == 0)
-							return
-						console.log(i,sel)
-						pieceKeys=pieceKeys.map((x: any, j: any) => {
-							return x.map((y: any, k: any) => (y==i?-1:y==sel?i:y));
+									<rect
+										x={(i % 8) * 80 + 32}
+										y={592 - Math.floor(i / 8) * 80}
+										width="16"
+										height="16"
+										rx="999"
+										fill={theme.move}
+										className="duration-200 pointer-events-none"
+										opacity={moveableSquares[i]}
+									/>
+								}
+								
+							</>
+						);
+					})}
+				</svg>
+				{pieceKeys.map((x: any, j: any) => {
+					return x.map((k: any, i: any) => {
+						return (
+							<>
+								{pieces[
+									`${j < 6 ? "0" : "1"}${(
+										j % 6
+									).toString()}` as keyof typeof pieces
+								]({
+									onMouseDown: (e: any) => {
+										if (
+											!(
+												(turn && j < 6) ||
+												(!turn && j >= 6)
+											)
+										)
+											return;
+										mvSq =
+											moveManager(k, board, turn) ||
+											new Array(64).fill(0);
+										setMoveableSquares(mvSq);
+										e.currentTarget.style.pointerEvents =
+											"none";
+										e.currentTarget.style.zIndex =
+											clickCount + 1;
+										setClickCount(clickCount + 1);
+										sel = k;
+										md = true;
+										pc =
+											`${j < 6 ? "0" : "1"}${(
+												j % 6
+											).toString()}` + i;
+									},
 
-						});
-						setMoveableSquares(new Array(64).fill(0));
-						turn=!turn;
-						let temp=board.split("");
-						temp[i]=temp[sel];
-						temp[sel]="-";
-						board=temp.join("");
-						setPieceID("null");
-						
-                    //   let temp = [...tempblank];
-                    //   if (temp[63 - i] == "1") {
-                    //     temp[63 - i] = "0";
-                    //   } else {
-                    //     temp[63 - i] = "1";
-                    //   }
-                    //   setTempBlank(temp);
-                    }}
-                  />
-                }
-              </>
-            );
-          })}
-        </svg>
+									id:
+										`${j < 6 ? "0" : "1"}${(
+											j % 6
+										).toString()}` + i,
+									style: boardPositionToGlobalPosition(k),
 
-        <div
-          className="text-white absolute right-0 bottom-0"
-          onClick={() => {
-            let temp = [];
-            let y = "";
-            tempblank.map((x, i) => {
-              if (i % 8 == 0 && i !== 0) {
-                temp.push(y);
-                y = "";
-              }
-              y = y + x;
-            });
-            temp.push(y);
-            console.log(parseInt(temp.join(""), 2).toString());
+									className: "duration-200 fixed z-[0]",
+									width: "8vmin",
+									height: "8vmin",
+								})}
+
+							</>
+						);
+					});
+				})}
+        <svg
+					id="mainboard2"
+					xmlns="http://www.w3.org/2000/svg"
+					className=" absolute h-[75vmin] aspect-square tms pointer-events-none duration-200  mb-0 "
+					viewBox="0 0 640 640"
+          style={{
+            zIndex:clickCount-1
           }}
-        >
-          test
-        </div>
-        {pieceKeys.map((x: any, j: any) => {
-          return x.map((k: any, i: any) => {
-            return (
-              <>
-                {pieces[
-                  `${j < 6 ? "0" : "1"}${(
-                    j % 6
-                  ).toString()}` as keyof typeof pieces
-                ]({
-                  onMouseDown: (e: any) => {
-                    if (!((turn && j < 6) || (!turn && j >= 6))) return;
-                    setMoveableSquares(moveManager(k, board, turn));
-                    e.currentTarget.style.zIndex = clickCount + 1;
-                    setClickCount(clickCount + 1);
-                    setDownPiece(k);
-					sel=k
-                    md = true;
-                    pc = `${j < 6 ? "0" : "1"}${(j % 6).toString()}` + i
-					setPieceID(pc);
-                  },
-
-                  id: `${j < 6 ? "0" : "1"}${(j % 6).toString()}` + i,
-                  style: boardPositionToGlobalPosition(k),
-
-                  className: "duration-200 fixed z-[0]",
-                  width: "8vmin",
-                  height: "8vmin",
-                })}
-              </>
-            );
-          });
-        })}
-      </div>
-    </>
-  );
+					fill="none">
+					{moveableSquares.map((_, i) => {
+						return (
+							<>
+                {
+									<rect
+										x={(i % 8) * 80 + 32}
+										y={592 - Math.floor(i / 8) * 80}
+										width="16"
+										height="16"
+										rx="999"
+										fill={theme.move}
+										className="duration-200 pointer-events-none"
+										opacity={moveableSquares[i]}
+									/>
+								}
+								
+							</>
+						);
+					})}
+				</svg>
+        
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/PieceMoves.tsx
+++ src/PieceMoves.tsx
@@ -1,7 +1,8 @@
 const pieceDict: { [key: string]: (k: any, board: any,enemy:any,empty:any, turn: boolean) => any } = {
-	p:pawnMoves,n:knightMoves,
+	p:pawnMoves,n:knightMoves,b:bishopMoves,r:rookMoves,q:queenMoves,k:kingMoves
 };
 export function moveManager(k:any,board:any,turn:boolean){
+	checkForCheck(board,turn)
 	let piece = board[k]
 	let pieceColor = piece<"a"
 	if(turn !== pieceColor)
@@ -37,7 +38,7 @@ function pawnMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
 	
 }
 function knightMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
-	console.log("called")
+	let moveable=enemy.map((x:any,i:any)=>x.map((y:any,j:any)=>y||empty[i][j]))
 	let moves=[]
 	let i = Math.floor(k/8)
 	let j = k%8
@@ -45,8 +46,88 @@ function knightMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
 	for(let x of directions){
 		let i2 = i+x[0]
 		let j2 = j+x[1]
-		if(i2>=0 && i2<8 && j2>=0 && j2<8 && (empty[i2][j2] || enemy[i2][j2]))
+		if(i2>=0 && i2<8 && j2>=0 && j2<8 && moveable[i2][j2])
 			moves.push(i2*8+j2)
 	}
 	return moves
+}
+function bishopMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
+	let moves=[]
+	let moveable=enemy.map((x:any,i:any)=>x.map((y:any,j:any)=>y||empty[i][j]))
+	let i = Math.floor(k/8)
+	let j = k%8
+	let directions = [[1,1],[1,-1],[-1,1],[-1,-1]]
+	for(let x of directions){
+		let i2 = i+x[0]
+		let j2 = j+x[1]
+		while(i2>=0 && i2<8 && j2>=0 && j2<8 && moveable[i2][j2]){
+			moves.push(i2*8+j2)
+			if(!empty[i2][j2])
+				break
+			i2+=x[0]
+			j2+=x[1]
+			
+		}
+	}
+	return moves
+	
+}
+function rookMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
+	let moves=[]
+	let moveable=enemy.map((x:any,i:any)=>x.map((y:any,j:any)=>y||empty[i][j]))
+	let i = Math.floor(k/8)
+	let j = k%8
+	let directions = [[1,0],[0,-1],[-1,0],[0,1]]
+	for(let x of directions){
+		let i2 = i+x[0]
+		let j2 = j+x[1]
+		while(i2>=0 && i2<8 && j2>=0 && j2<8 && moveable[i2][j2]){
+			moves.push(i2*8+j2)
+			if(!empty[i2][j2])
+				break
+			i2+=x[0]
+			j2+=x[1]
+			
+		}
+	}
+	return moves
+	
+}
+function queenMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
+	let moves=bishopMoves(k,board,enemy,empty,turn).concat(rookMoves(k,board,enemy,empty,turn))
+	return moves
+}
+function kingMoves(k: any, board: any,enemy:any,empty:any, turn: boolean) {
+	let moves=[]
+	let moveable=enemy.map((x:any,i:any)=>x.map((y:any,j:any)=>y||empty[i][j]))
+	let i = Math.floor(k/8)
+	let j = k%8
+	let directions = [[1,0],[0,-1],[-1,0],[0,1],[1,1],[1,-1],[-1,1],[-1,-1]]
+	for(let x of directions){
+		let i2 = i+x[0]
+		let j2 = j+x[1]
+		if(i2>=0 && i2<8 && j2>=0 && j2<8 && moveable[i2][j2])
+			moves.push(i2*8+j2)
+	}
+	return moves
+}
+
+function checkForCheck(board:any,turn:boolean){
+	let boardMatrix = "00000000".split("").map((_,i)=>board.slice(i*8,(i+1)*8).split(""))
+	let emptySpaces = boardMatrix.map((x)=>x.map((y:any)=>y=="-"?true:false))
+	let myPieces = boardMatrix.map((x)=>x.map((y:any)=>((turn?y.toUpperCase():y.toLowerCase())==y) && y!= "-"?true:false))
+	let kingIndex = board.indexOf((turn?"K":"k"))
+	let check=false
+	for(let i = 0; i<64&&!check;i++){
+		if(board[i]=="-")
+			continue
+		else if(turn&&board[i]>="a"){
+			check=pieceDict[board[i].toLowerCase()](i,boardMatrix,myPieces,emptySpaces,!turn).includes(kingIndex)
+		}
+		else if(!turn&&board[i]<="Z"){check=pieceDict[board[i].toLowerCase()](i,boardMatrix,myPieces,emptySpaces,!turn).includes(kingIndex)}
+		
+	}
+	if(check)
+		console.log("CHECK")
+	
 }
